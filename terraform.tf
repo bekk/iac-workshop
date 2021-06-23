3928f10eb7d03f04b6880950947168f8fdba47ce
Even Holthe <even.holthe@bekk.no>
2021-06-23 09:52:59 +0200

    provider: random

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,21 @@
+# TODO: Backend?
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
+      source  = "hashicorp/azurerm"
+      version = "2.64.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
     }
   }
 }
 
 provider "azurerm" {
+  tenant_id       = "ef45594d-07dd-4e5e-bc95-0bc6b361e482"
+  subscription_id = "9539bc24-8692-4fe2-871e-3733e84b1b73"
   features {}
 }
 

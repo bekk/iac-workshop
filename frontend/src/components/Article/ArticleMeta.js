5e8ad0c5b6407272bc5474f0633dcefcd4340489
Ole Kristian Pedersen <okpedersen@gmail.com>
2021-06-23 14:50:35 +0200

    Add frontend

---
+++ frontend/src/components/Article/ArticleMeta.js
@@ -0,0 +1,27 @@
+import ArticleActions from './ArticleActions';
+import { Link } from 'react-router-dom';
+import React, {memo} from 'react';
+
+const ArticleMeta = props => {
+  const article = props.article;
+  return (
+    <div className="article-meta">
+      <Link to={`/@${article.author.username}`}>
+        <img src={article.author.image || 'https://static.productionready.io/images/smiley-cyrus.jpg'} alt={article.author.username} />
+      </Link>
+
+      <div className="info">
+        <Link to={`/@${article.author.username}`} className="author">
+          {article.author.username}
+        </Link>
+        <span className="date">
+          {new Date(article.createdAt).toDateString()}
+        </span>
+      </div>
+
+      <ArticleActions canModify={props.canModify} article={article} />
+    </div>
+  );
+};
+
+export default memo(ArticleMeta);

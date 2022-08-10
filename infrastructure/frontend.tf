24124d3643a20c97a1e090552f37721d67f44a48
Ole Kristian Pedersen <okpedersen@gmail.com>
2022-08-10 08:32:13 +0200

    Upgrade azurerm provider -> 3.17

--- infrastructure/frontend.tf
+++ infrastructure/frontend.tf
@@ -22,14 +22,14 @@ locals {
 }
 
 resource "azurerm_storage_account" "web" {
-  name                      = local.unique_id_sanitized
-  resource_group_name       = azurerm_resource_group.rg.name
-  location                  = azurerm_resource_group.rg.location
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  allow_blob_public_access  = true # Be aware that this is not always what you want, this gives anyone with the url access
-  enable_https_traffic_only = true
-  min_tls_version           = "TLS1_2"
+  name                            = local.unique_id_sanitized
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = true # Be aware that this is not always what you want, this gives anyone with the url access
+  enable_https_traffic_only       = true
+  min_tls_version                 = "TLS1_2"
 
   static_website {
     index_document = "index.html"

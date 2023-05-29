6938640d0848a159f70a751b09b570954b9411be
Ole Kristian Pedersen <okpedersen@gmail.com>
2023-05-29 22:29:55 +0200

    Solutions: rettiprod.live -> cloudlabs-azure.no

--- infrastructure/dns.tf
+++ infrastructure/dns.tf
@@ -4,24 +4,24 @@ locals {
 }
 
 // If the workshop is run externally, these values should be changed
-data "azurerm_dns_zone" "rettiprod_live" {
-  name                = "rettiprod.live"
+data "azurerm_dns_zone" "cloudlabs_azure_no" {
+  name                = "cloudlabs-azure.no"
   resource_group_name = "workshop-admin"
 }
 
 resource "azurerm_dns_cname_record" "www" {
-  zone_name           = data.azurerm_dns_zone.rettiprod_live.name
-  resource_group_name = data.azurerm_dns_zone.rettiprod_live.resource_group_name
+  zone_name           = data.azurerm_dns_zone.cloudlabs_azure_no.name
+  resource_group_name = data.azurerm_dns_zone.cloudlabs_azure_no.resource_group_name
 
   ttl    = 60
   name   = local.unique_id_raw
   record = "${azurerm_cdn_endpoint.cdn_endpoint.name}.azureedge.net"
 }
 
-resource "azurerm_cdn_endpoint_custom_domain" "rettiprod_custom_domain" {
+resource "azurerm_cdn_endpoint_custom_domain" "cloudlabs_custom_domain" {
   name            = local.unique_id_raw
   cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
-  host_name       = "${local.unique_id_raw}.${data.azurerm_dns_zone.rettiprod_live.name}"
+  host_name       = "${local.unique_id_raw}.${data.azurerm_dns_zone.cloudlabs_azure_no.name}"
 
   cdn_managed_https {
     certificate_type = "Dedicated"
